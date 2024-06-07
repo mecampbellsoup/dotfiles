d62c979e02780467047bb542e60998ebde472f69
Matt Campbell <mecampbell25@gmail.com>
2024-06-07 13:11:01 -0400

    Update .fzf.bash

--- dot_fzf.bash
+++ dot_fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */home/mcampbell/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/mcampbell/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+# source "/home/mcampbell/.fzf/shell/completion.bash"
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+# source "/home/mcampbell/.fzf/shell/key-bindings.bash"
